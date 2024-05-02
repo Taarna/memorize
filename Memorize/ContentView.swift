34884254efe3fd4e22d8638365716ec8261193c3
Ivana <ivana.rast@gmail.com>
2024-05-02 22:30:34 +0200

    Assignment 1 (#2)
    * feat: add title
    
    * feat: add themes
    
    * refactor: remove add and remove buttons
    
    * fix: cards should come in pairs
    
    * refactor: use struct instead of func for theme buttons
    
    * refactor: move theme names to constants
    
    * fix: shuffle the cards when changing theme
    
    * fix: buttons must have icon and text
    
    * chore: minor layout changes
    
    * feat: add theme color

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,61 +7,109 @@
 
 import SwiftUI
 
+enum Themes {
+    enum ChristmasTheme {
+        static let name = "Xmas"
+        static let icon = "snowflake"
+        static let color: Color = .blue
+    }
+    enum HalloweenTheme {
+        static let name = "Halloween"
+        static let icon = "theatermasks.fill"
+        static let color: Color = .orange
+    }
+    enum AnimalsTheme {
+        static let name = "Animals"
+        static let icon = "pawprint.fill"
+        static let color: Color = .green
+    }
+}
+
+
 struct ContentView: View {
-    let emojis: [String] = ["🎃", "👻", "🕸️", "👿", "😍", "🦊", "🫣", "🤖", "🍆", "🍓", "🥎", "☎️"]
-    @State var cardCount: Int = 4
+    let emojisChristmas = ["🎄", "🎅", "❄️", "⛄️"]
+    let emojisHalloween = ["🎃", "👻", "🕸️", "👿", "🧟‍♀️"]
+    let emojisAnimals = ["🐮", "🐶", "🐱", "🦆", "🐵", "🦊"]
+    
+    @State var currentEmojis: [String] = []
+    @State var currentColor: Color = .green
     
     var body: some View {
         VStack {
+            Text("Memorize!").font(.largeTitle)
             ScrollView {
                 cards
             }
             Spacer()
-            cardCountAdjusters
+            themeChangers
         }
         .padding()
     }
     
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
-    
     var cards: some View {
         LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(content: emojis[index]).aspectRatio(2/3, contentMode: .fit)
+            ForEach(0..<currentEmojis.count, id: \.self) { index in
+                CardView(content: currentEmojis[index]).aspectRatio(2/3, contentMode: .fit)
             }
         }
-        .foregroundColor(.orange)
+        .foregroundColor(currentColor)
     }
     
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    var themeChangers: some View {
+        HStack(alignment: .bottom) {
+            ThemeButton(
+                text: Themes.ChristmasTheme.name,
+                imageName: Themes.ChristmasTheme.icon) { apply(theme: Themes.ChristmasTheme.name) }
+            ThemeButton(
+                text: Themes.HalloweenTheme.name,
+                imageName: Themes.HalloweenTheme.icon) { apply(theme: Themes.HalloweenTheme.name) }
+            ThemeButton(
+                text: Themes.AnimalsTheme.name,
+                imageName: Themes.AnimalsTheme.icon) { apply(theme: Themes.AnimalsTheme.name) }
+        }
     }
     
-    var cardRemover: some View {
-        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    struct ThemeButton: View {
+        var text: String
+        var imageName: String
+        var action: () -> Void
+        
+        var body: some View {
+            Button(action: {
+                action()
+            }, label: {
+                VStack {
+                    Image(systemName: imageName)
+                    Text(text).font(.caption)
+                }
+            })
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        }
     }
     
-    var cardAdder: some View {
-        return cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
+    func apply(theme: String) {
+        switch theme {
+        case Themes.ChristmasTheme.name :
+            currentEmojis = emojisChristmas + emojisChristmas
+            currentColor = Themes.ChristmasTheme.color
+        case Themes.HalloweenTheme.name:
+            currentEmojis = emojisHalloween + emojisHalloween
+            currentColor = Themes.HalloweenTheme.color
+        case Themes.AnimalsTheme.name:
+            currentEmojis = emojisAnimals + emojisAnimals
+            currentColor = Themes.AnimalsTheme.color
+        default:
+            currentEmojis = emojisAnimals + emojisAnimals
+            currentColor = Themes.AnimalsTheme.color
+        }
+        currentEmojis.shuffle()
     }
 }
 
 struct CardView: View {
     let content: String
-    @State var isFaceUp = true
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
