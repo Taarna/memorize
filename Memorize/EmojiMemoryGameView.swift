6f65e88bc056773772bd3f5f5d44815026187e85
Ivana <ivana.rast@gmail.com>
2024-05-07 14:53:22 +0200

    feat: add model and view model (#4)

---
+++ Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,61 @@
+//
+//  EmojiMemoryGameView.swift
+//  Memorize
+//
+//  Created by Ivana Rast on 29.04.2024..
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
+    var body: some View {
+        VStack {
+            Text("Memorize!").font(.largeTitle)
+            ScrollView {
+                cards
+            }
+            Button("Suffle") {
+                viewModel.shuffle()
+            }
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
+            ForEach(viewModel.cards.indices, id: \.self) { index in
+                CardView(viewModel.cards[index]).aspectRatio(2/3, contentMode: .fit).padding(4)
+            }
+        }
+    }
+}
+
+struct CardView: View {
+    let card: MemoryGame<String>.Card
+    
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
+    
+    var body: some View {
+        ZStack {
+            let baseCard = RoundedRectangle(cornerRadius: 12)
+            Group {
+                baseCard.fill(.white)
+                baseCard.strokeBorder(lineWidth: 2)
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
+            }
+            .opacity(card.isFaceUp ? 1 : 0)
+            baseCard.fill().opacity(card.isFaceUp ? 0 : 1)
+        }
+    }
+}
+
+#Preview {
+    EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+}
