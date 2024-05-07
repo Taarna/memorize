355b5da0ddfb620bc8d5c918dd83052e0eed637f
Ivana <ivana.rast@gmail.com>
2024-05-07 14:52:56 +0200

    feat: add model and view model

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,6 +7,30 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    var model: MemoryGame<String>
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["🎄", "🎅", "❄️", "⛄️"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 10) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+    
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+    
+    func shuffle() {
+        model.shuffle()
+    }
 }
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -8,15 +8,28 @@
 import Foundation
 
 struct MemoryGame<CardContent> {
-    var cards: Array<Card>
+    private(set) var cards: Array<Card>
     
-    func choose(card: Card) {
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        for pairIndex in 0..<max(2, numberOfPairsOfCards) {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
+    func choose(_ card: Card) {
         
     }
     
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    
     struct Card {
-        var isFaceUp: Bool
-        var isMatched: Bool
-        var content: CardContent
+        var isFaceUp = true
+        var isMatched = false
+        let content: CardContent
     }
 }
