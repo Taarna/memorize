30b1505b447e207257af5069887a7f87edcd4925
Ivana <ivana.rast@gmail.com>
2024-05-10 10:45:57 +0200

    chore: define themes

---
+++ Memorize/Theme.swift
@@ -0,0 +1,29 @@
+//
+//  Theme.swift
+//  Memorize
+//
+//  Created by Ivana Rast on 10.05.2024..
+//
+
+import Foundation
+
+struct Theme {
+    struct Animals {
+        let name = "Animals"
+        let emoji = ["🐶","🐱","🐭","🦊","🐸","🐤"]
+        let numberOfPairs = 4
+        let color = "green"
+    }
+    struct Vehicles {
+        let name = "Vehicles"
+        let emoji = ["🚗","🚙","🚎","🛺","🚢","✈️", "🏎️"]
+        let numberOfPairs = 7
+        let color = "red"
+    }
+    struct Food {
+        let name = "Food"
+        let emoji = ["🫐","🥨","🍔","🍖"]
+        let numberOfPairs = 10
+        let color = "blue"
+    }
+}
