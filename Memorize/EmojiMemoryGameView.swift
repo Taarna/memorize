40e8b104c68cbe851896ed6fa66821eb2069b4d7
Ivana <ivana.rast@gmail.com>
2024-05-24 13:52:00 +0200

    Lecture 6

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -2,13 +2,15 @@
 //  EmojiMemoryGameView.swift
 //  Memorize
 //
-//  Created by Ivana Rast on 29.04.2024..
+//  Created by Ivana Rast on 29.04.2024.
 //
 
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
+    
+    let cardAspectRatio: CGFloat = 2/3
     
     var body: some View {
         VStack {
@@ -24,18 +26,16 @@ struct EmojiMemoryGameView: View {
         .padding()
     }
     
-    var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards) { card in
-                CardView(card, themeColor: viewModel.themeColor)
-                    .aspectRatio(2/3, contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
-                    }
-            }
+    private var cards: some View {
+        AspectVGrid(viewModel.cards, aspectRatio: cardAspectRatio) { card in
+            CardView(card, themeColor: viewModel.themeColor)
+                .padding(4)
+                .onTapGesture {
+                    viewModel.choose(card)
+                }
         }
     }
+
 }
 
 struct CardView: View {
--- Memorize/Theme.swift
+++ Memorize/Theme.swift
@@ -19,6 +19,7 @@ struct Themes {
     private static let Vehicles = Theme(name: "Vehicles", emojis: ["🚗","🚙","🚎","🛺","🚢","✈️", "🏎️"], numberOfPairs: 7, color: "red")
     private static let Food = Theme(name: "Food", emojis: ["🫐","🥨","🍔","🍖", "🍕", "🍤", "🍰", "🍡", "🌮"], numberOfPairs: 8, color: "blue")
     private static let Flags = Theme(name: "Flags", emojis: ["🇧🇹", "🇧🇮", "🇨🇰"], numberOfPairs: 2, color: "orange")
+    private static let Sports = Theme(name: "Sports", emojis: ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🏐", "🏉", "🏓", "🥊", "⛳️", "🏑", "🛼"], numberOfPairs: 2, color: "orange")
     
     private static var allThemes: Array<Theme> {
         [Animals, Vehicles, Food, Flags]
